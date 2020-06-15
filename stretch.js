0f7dc854fa9336bec5d7f352b63e551ae6c3d06b
richard-mcghee <richardjmcghee@yahoo.com>
2020-06-15 18:51:22 -0400

    completed stretch 2

--- stretch.js
+++ stretch.js
@@ -1,3 +1,5 @@
+// Stretch 1
+
 // (function(){
 //     var a = b = 3;
 //   })();
@@ -5,3 +7,13 @@
 //   console.log("b defined? " + (typeof b !== 'undefined'));
 
   // A is undefined because it is defined as B but then b is immediately redefined to 3 which makes it it's own variable and no longer related A, I believe
+
+//   Stretch 2
+
+function addSix(num){
+    let base = 6;
+    return console.log(base += num);
+}
+
+addSix(10);
+addSix(21);
